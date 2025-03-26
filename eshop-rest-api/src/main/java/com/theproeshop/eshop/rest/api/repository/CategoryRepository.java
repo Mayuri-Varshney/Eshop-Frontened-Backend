c07a74fabc16ea182e1ce4ecb37729b060e14687
Mayuri-Varshney <155051840+Mayuri-Varshney@users.noreply.github.com>
2025-03-27 00:03:33 +0530

    Delete eshop-rest-api directory

--- eshop-rest-api/src/main/java/com/theproeshop/eshop/rest/api/repository/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.theproeshop.eshop.rest.api.repository;
-
-import com.theproeshop.eshop.rest.api.entity.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
