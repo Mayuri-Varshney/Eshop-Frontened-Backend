7829cedb78ec984d4593b25e0bb6facc3c5587de
Mayuri Varshney <varshneymayuri4@gmail.com>
2025-03-27 11:54:14 +0530

    Initial commit

---
+++ src/test/java/com/theproeshop/rest/api/eshop/RestApiEshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.theproeshop.rest.api.eshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiEshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
